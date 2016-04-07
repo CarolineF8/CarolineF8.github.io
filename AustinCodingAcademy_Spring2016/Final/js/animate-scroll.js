a68461e5132221f50d6a9a31b295f653c50384f2
CarolineF8 <carolinebfaith@gmail.com>
2016-04-07 15:47:02 -0500

    hover window

--- AustinCodingAcademy_Spring2016/Final/js/animate-scroll.js
+++ AustinCodingAcademy_Spring2016/Final/js/animate-scroll.js
@@ -12,4 +12,42 @@ $(document).ready(function () {
 
     });
 
+
+    $('.zoompic1').hover(function () {
+
+        $('.bigpic1').removeClass('flyaway');
+        $('.bigpic2').addClass('flyaway');
+        $('.bigpic3').addClass('flyaway');
+        $('.bigpic4').addClass('flyaway');
+
+
+    });
+    $('.zoompic2').hover(function () {
+
+        $('.bigpic2').removeClass('flyaway');
+        $('.bigpic1').addClass('flyaway');
+        $('.bigpic3').addClass('flyaway');
+        $('.bigpic4').addClass('flyaway');
+
+
+    });
+    $('.zoompic3').hover(function () {
+
+        $('.bigpic3').removeClass('flyaway');
+        $('.bigpic2').addClass('flyaway');
+        $('.bigpic1').addClass('flyaway');
+        $('.bigpic4').addClass('flyaway');
+
+
+    });
+    $('.zoompic4').hover(function () {
+
+        $('.bigpic4').removeClass('flyaway');
+        $('.bigpic2').addClass('flyaway');
+        $('.bigpic3').addClass('flyaway');
+        $('.bigpic1').addClass('flyaway');
+
+
+    });
+
 });
